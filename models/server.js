5dbe16cdc9a7ba627133e46600d4ef6c440cf89a
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2021-10-03 13:24:09 -0300

    middlewares configurados y llamada al server

--- models/server.js
+++ models/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 
 
 
@@ -8,9 +9,40 @@ class Server{
     this.app = express();
     this.port = process.env.PORT;
     this.path ={
-      //path:"route"
+      //path-e:"endpoint"
     };
+
+    //base de datos
+
+    //middlewares
+
+    //rutas de mi app
+
+
    }
 
 
+   middlewares(){
+    //cors
+    this.app.use(cors());
+
+    //lectura y parseo del body
+    this.app.use(express.json());
+
+    //directorio publico
+    this.app.use(express.static("public"));
+
+   }
+
+
+   routes(){
+    // this.app.use(this.path.path-e, require("route"));
+   }
+
+   listen(){
+    this.app.listen(this.port);
+   }
+
 }
+
+module.exports = Server;
